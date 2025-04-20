9b89e620c8ffcaf55d8f044457d89aec430b1070
John Walthour <john.walthour@gmail.com>
2025-04-19 22:26:12 -0400

    real simple PoC that only works once per repo

---
+++ update_repos.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+from git import Repo
+import logging
+import os
+import yaml
+
+logger = logging.getLogger(__name__)
+
+SYS_SETTINGS_FN = "system_settings.yaml"
+REPO_LIST_FN = "repo_listing.yaml"
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    with open(SYS_SETTINGS_FN) as stream:
+        try:
+            sys_settings = yaml.safe_load(stream)
+        except yaml.YAMLError:
+            sys_settings = {}
+            logger.error(exc_info=True)
+    with open(REPO_LIST_FN) as stream:
+        try:
+            repo_list = yaml.safe_load(stream)
+        except yaml.YAMLError:
+            repo_list = []
+            logger.error(exc_info=True)
+
+    def clone_progress(op_code, cur_count, max_count=None, message=''):
+        logger.info(f"Progress: {op_code}, {cur_count}/{max_count}: {message}")
+
+    storage_root = sys_settings["storage_root"]
+    for repo in repo_list:
+        url = repo["url"]
+        protocol,path=url.split("://")
+        path_elements = path.split("/")
+        path_elements_sanitized = [el for el in path_elements if el != ".."]
+        local_repo_path = os.path.join(storage_root, *path_elements_sanitized)
+        logger.info(f"Will clone {repo} into {local_repo_path}")
+        os.makedirs(local_repo_path, exist_ok=True)
+        repo = Repo.clone_from(url, local_repo_path, clone_progress, multi_options=["--bare"])
+    
